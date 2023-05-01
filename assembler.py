e859585ce01a9df8dd7bf7140225eb8aee88be2d
rayyanhussain2 <114013347+rayyanhussain2@users.noreply.github.com>
2023-05-01 16:02:09 +0530

    Added code for label allocation
    Wrote the code for allocating binary addresses for labels and storing it in the label_dict. Also temporarily commented out argv as an input for developing convenience. We shall deal with argv and writing output towards the end.

--- assembler.py
+++ assembler.py
@@ -7,7 +7,9 @@ register_dict={'R0':'000','R1':'001','R2':'010','R3':'011','R4':'100','R5':'101'
 
 
 
-req_file=sys.argv[1]
+#req_file=sys.argv[1]
+req_file='input1.txt'
+
 
 input_file=open(req_file,'r')
 input_assembly_codes=input_file.read().splitlines()
@@ -20,9 +22,13 @@ for i in range (len(input_assembly_codes)):
         input_assembly_codes.append(input_assembly_codes[i])
 
 
-
-
-
-for i in range(len(input_assembly_codes)):
-    print(input_assembly_codes[i])
-
+label_dict = {}
+#Reading for label allocation
+for i in range(instruction_counter):
+    if(input_assembly_codes[i].split()[0][-1] == ':'):
+        temp = str(bin(i)[2:])
+        temp = "0"*(7 - len(temp)) + temp
+        label_dict[input_assembly_codes[i].split()[0][:-1]] = str(temp)
+        temp = input_assembly_codes[i].split()
+        temp = " ".join(temp[1:])
+        input_assembly_codes[i] = temp
