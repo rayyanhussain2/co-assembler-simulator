13e5a0f1d530bdf6fd3fc1c215b6a40d15e53070
rayyanhussain2 <rayyanhussain7@proton.me>
2023-05-07 22:57:46 +0530

    Wrote code to handle error cases a, b and c

--- assembler.py
+++ assembler.py
@@ -4,7 +4,7 @@ op_dict = {'add': ['00000', 'A'], 'sub': ['00001', 'A'], 'mov': ['00010', 'B'], 
           'ld': ['00100', 'D'], 'st': ['00101', 'D'], 'mul': ['00110', 'A'], 'div': ['00111', 'C'],
           'rs': ['01000', 'B'], 'ls': ['01001', 'B'], 'xor': ['01010', 'A'], 'or': ['01011', 'A'],
           'and': ['01100', 'A'], 'not': ['01101', 'C'], 'cmp': ['01110', 'C'], 'jmp': ['01111', 'E'],
-          'jlt': ['10000', 'E'], 'jgt': ['10001', 'E'], 'je': ['10010', 'E'], 'hlt': ['10011', 'F']}
+          'jlt': ['10000', 'E'], 'jgt': ['10001', 'E'], 'je': ['10010', 'E'], 'hlt': ['10011', 'F'], 'var': ['', '']}
 
 inst_list = ['add', 'sub', 'mov', 'ld', 'st', 'mul', 'div', 'rs', 'ls', 'xor', 'or', 'and', 'not', 'cmp',
              'jmp', 'jlt', 'jgt', 'je', 'hlt']
@@ -53,9 +53,14 @@ for i in range(instruction_counter):
         temp = " ".join(temp[1:])
         input_assembly_codes[i] = temp
 
-#reading through the list of assembly codes and converting to machine
+#Conversion of assembly to binary
 instruction_size=16
 machine_code_list=[]
+#creating a 2d list to store the indices of the instructions that have errors in them.
+#each list within is of the format [index, message, error_type]
+error_indices = []
+
+#reading through the list of assembly codes and converting to machine
 for i in range(len(input_assembly_codes)):
     #Handling the case where the opcodes are valid, else would be the error handling
     #For now, we store the machine code in a list.
@@ -63,44 +68,112 @@ for i in range(len(input_assembly_codes)):
     if curr_line[0] in op_dict:
         #If opcode is of type A
         if op_dict[curr_line[0]][1]=='A':
-            unused_bits=instruction_size-(3*len(register_dict['R0'])+len(op_dict[curr_line[0]][0]))
-            beginning_bits=op_dict[curr_line[0]][0]+unused_bits*'0'
-            register_bits=register_dict[curr_line[1]]+register_dict[curr_line[2]]+register_dict[curr_line[3]]
-            machine_code_list.append(beginning_bits+register_bits)
+            #If the registers are valid
+            if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys() and curr_line[3] in register_dict.keys()):
+                unused_bits=instruction_size-(3*len(register_dict['R0'])+len(op_dict[curr_line[0]][0]))
+                beginning_bits=op_dict[curr_line[0]][0]+unused_bits*'0'
+                register_bits=register_dict[curr_line[1]]+register_dict[curr_line[2]]+register_dict[curr_line[3]]
+                machine_code_list.append(beginning_bits+register_bits)
+            else:
+                #creating a string var to store the error message which contains all the registers that are faulty.
+                #This message will be later parsed and printed out as required.
+                error_message = ""
+                if(curr_line[1] not in register_dict.keys()):
+                    error_message += curr_line[1] + " "
+                if(curr_line[2] not in register_dict.keys()):
+                    error_message += curr_line[2] + " "
+                if(curr_line[3] not in register_dict.keys()):
+                    error_message += curr_line[3] + " "
+                error_indices.append([i, error_message, 'a_reg'])
 
         #If opcode is of type B
         elif op_dict[curr_line[0]][1] == 'B' and curr_line[2] not in register_dict.keys():
-            unused_bits = "0" * (1)
-            immidiate_value = str(bin(int(curr_line[2][1:])))
-            immidiate_value = "0" * (7-len(immidiate_value[2:])) + immidiate_value[2:]
-            machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + immidiate_value)
+            #If the registers are valid
+            if(curr_line[1] in register_dict.keys()):
+                unused_bits = "0" * (1)
+                immidiate_value = str(bin(int(curr_line[2][1:])))
+                immidiate_value = "0" * (7-len(immidiate_value[2:])) + immidiate_value[2:]
+                machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + immidiate_value)
+            else:
+                error_indices.append([i, curr_line[1], 'a_reg'])
        
         #If opcode is of type D
         elif op_dict[curr_line[0]][1] == 'D':
-            unused_bits = "0" * (1)
-            machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + var_dict[curr_line[2]])
+            #If the registers are valid and variables are valid
+            if(curr_line[1] in register_dict.keys() and curr_line[2] in variable_dict.keys()):
+                unused_bits = "0" * (1)
+                machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + var_dict[curr_line[2]])
+            else:
+                #if the error lies in register
+                if(curr_line[1] not in register_dict.keys()):
+                    error_indices.append([i, curr_line[1], 'a_reg'])
+                #if the error lies in variable
+                if(curr_line[2] not in variable_dict.keys()):
+                    error_indices.append([i, curr_line[2], 'b'])
        
         #if opcode is of type C
         elif curr_line[0] == 'mov' and curr_line[2] in register_dict.keys():
-            unused_bits = "0" * (5)
-            machine_code_list.append(op_dict['mov_reg'][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])   
+            #If the registers are valid
+            if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
+                unused_bits = "0" * (5)
+                machine_code_list.append(op_dict['mov_reg'][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])   
+            else:
+                #creating a string var to store the error message which contains all the registers that are faulty.
+                #This message will be later parsed and printed out as required.
+                error_message = ""
+                if(curr_line[1] not in register_dict.keys()):
+                    error_message += curr_line[1] + " "
+                if(curr_line[2] not in register_dict.keys()):
+                    error_message += curr_line[2] + " "
+                error_indices.append([i, error_message, 'a_reg'])
+
         elif op_dict[curr_line[0]][1] == 'C' :
-            unused_bits = "0" * (5)
-            machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])
+            #If the registers are valid
+            if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
+                unused_bits = "0" * (5)
+                machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])
+            else:
+                #creating a string var to store the error message which contains all the registers that are faulty.
+                #This message will be later parsed and printed out as required.
+                error_message = ""
+                if(curr_line[1] not in register_dict.keys()):
+                    error_message += curr_line[1] + " "
+                if(curr_line[2] not in register_dict.keys()):
+                    error_message += curr_line[2] + " "
+                error_indices.append([i, error_message, 'a_reg'])
         
         #if op code is of type E
         elif op_dict[curr_line[0]][1] == 'E':
-            unused_bits = "0" * (4)
-            op = op_dict[curr_line[0]][0]
-            mem_ = label_dict[curr_line[1]]           
-            s = op + unused_bits + mem_
-            machine_code_list.append(s)
+            if(curr_line[1] in label_dict.keys()):
+                unused_bits = "0" * (4)
+                op = op_dict[curr_line[0]][0]
+                mem_ = label_dict[curr_line[1]]           
+                s = op + unused_bits + mem_
+                machine_code_list.append(s)
+            else:
+                error_indices.append([i, curr_line[1], 'c'])
        
         #if op code is of type F
         elif op_dict[curr_line[0]][1] == 'F':
             s = "1001100000000000"
             machine_code_list.append(s)
+    #Handling case where there are typos in instruction name
+    else:
+        error_indices.append([i, "Invalid operation name: " + curr_line[0] , 'a_opcode'])
+
+if(len(error_indices) == 0):
+    pass
+    #code for writing the content of machine_code_list into text file - Prabal. Delete the pass statement
+else:
+    pass
+    #code to print error
 
 
 #For Debug purpose, shall delete later
 print(machine_code_list)
+print()
+print(variable_dict.keys())
+print()
+print(register_dict.keys())
+print()
+print(error_indices)
