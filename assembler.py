79cb6bd8928bfa1a7a0cc4ddb3201124449f756b
rayyanhussain2 <rayyanhussain7@proton.me>
2023-05-13 23:24:40 +0530

    Fixed issue #10

--- assembler.py
+++ assembler.py
@@ -14,7 +14,7 @@ register_dict={'R0':'000','R1':'001','R2':'010','R3':'011','R4':'100','R5':'101'
 
 
 #req_file=sys.argv[1]
-req_file='input15.txt'
+req_file='input1.txt'
 
 
 input_file=open(req_file,'r')
@@ -61,38 +61,106 @@ error_indices = []
 hlt_flag=False
 used_variables = list()
 hlt_counter = 0
-#reading through the list of assembly codes and converting to machine
-for i in range(len(input_assembly_codes)):
-    #Handling the case where the opcodes are valid, else would be the error handling
-    #For now, we store the machine code in a list.
-    curr_line = input_assembly_codes[i].split()
-    if len(curr_line) == 3:
-        if curr_line[2] == "FLAGS" and curr_line[0] != "mov":
-            error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
-            continue
 
-    elif len(curr_line) == 2:
-        if curr_line[1] == "FLAGS":
-            error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
-            continue
+#Handling instruction length errors
+if(len(input_assembly_codes) > 128):
+    error_indices.append([128,'Instruction length exceeds memory size',''])
 
-    elif len(curr_line) == 4:
-        if "FLAGS" in curr_line:
-            error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
-            continue
+#reading through the list of assembly codes and converting to machine and handling errors
+else:
+    for i in range(len(input_assembly_codes)):
+        #Handling the case where the opcodes are valid, else would be the error handling
+        #For now, we store the machine code in a list.
+        curr_line = input_assembly_codes[i].split()
+        if len(curr_line) == 3:
+            if curr_line[2] == "FLAGS" and curr_line[0] != "mov":
+                error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
+                continue
 
-    
-    if curr_line[0] in op_dict:
-        #If opcode is of type A
+        elif len(curr_line) == 2:
+            if curr_line[1] == "FLAGS":
+                error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
+                continue
 
-        if op_dict[curr_line[0]][1]=='A':
-            if len(curr_line)==4:
+        elif len(curr_line) == 4:
+            if "FLAGS" in curr_line:
+                error_indices.append([str(i),"ILLEGAL USE OF FLAGS REGISTER", "d"])
+                continue
+
+        
+        if curr_line[0] in op_dict:
+            #If opcode is of type A
+
+            if op_dict[curr_line[0]][1]=='A':
+                if len(curr_line)==4:
+                    #If the registers are valid
+                    if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys() and curr_line[3] in register_dict.keys()):
+                        unused_bits=instruction_size-(3*len(register_dict['R0'])+len(op_dict[curr_line[0]][0]))
+                        beginning_bits=op_dict[curr_line[0]][0]+unused_bits*'0'
+                        register_bits=register_dict[curr_line[1]]+register_dict[curr_line[2]]+register_dict[curr_line[3]]
+                        machine_code_list.append(beginning_bits+register_bits)
+                    else:
+                        #creating a string var to store the error message which contains all the registers that are faulty.
+                        #This message will be later parsed and printed out as required.
+                        error_message = ""
+                        if(curr_line[1] not in register_dict.keys()):
+                            error_message += curr_line[1] + " "
+                        if(curr_line[2] not in register_dict.keys()):
+                            error_message += curr_line[2] + " "
+                        if(curr_line[3] not in register_dict.keys()):
+                            error_message += curr_line[3] + " "
+                        error_indices.append([str(i), error_message, 'a_reg'])
+                else:
+                    error_indices.append([str(i),"general syntax error"])
+
+            #If opcode is of type B
+            elif op_dict[curr_line[0]][1] == 'B' and curr_line[2] not in register_dict.keys():
+                imm_flag=True
+                if(eval(curr_line[2][1:])!=int or int(curr_line[2][1:])<0 or int(curr_line[2][1:])>= (2**7)):
+                    error_message="illegal immediate value"
+                    error_indices.append([str(i),error_message,'e'])
+                    imm_flag=False
                 #If the registers are valid
-                if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys() and curr_line[3] in register_dict.keys()):
-                    unused_bits=instruction_size-(3*len(register_dict['R0'])+len(op_dict[curr_line[0]][0]))
-                    beginning_bits=op_dict[curr_line[0]][0]+unused_bits*'0'
-                    register_bits=register_dict[curr_line[1]]+register_dict[curr_line[2]]+register_dict[curr_line[3]]
-                    machine_code_list.append(beginning_bits+register_bits)
+                if(curr_line[1] in register_dict.keys()):
+                    if imm_flag==True:
+                        unused_bits = "0" * (1)
+
+                        
+
+                        immidiate_value = str(bin(int(curr_line[2][1:])))
+                        immidiate_value = "0" * (7-len(immidiate_value[2:])) + immidiate_value[2:]
+                        machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + immidiate_value)
+                    else:
+                        pass
+                else:
+                    error_indices.append([str(i), curr_line[1], 'a_reg'])
+        
+            #If opcode is of type D
+            elif op_dict[curr_line[0]][1] == 'D':
+                #If the registers are valid and variables are valid
+                if(curr_line[1] in register_dict.keys() and curr_line[2] in variable_dict.keys()):
+                    unused_bits = "0" * (1)
+                    machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + variable_dict[curr_line[2]])
+                else:
+                    #if the error lies in register
+                    if(curr_line[1] not in register_dict.keys()):
+                        error_indices.append([str(i), curr_line[1], 'a_reg'])
+                
+                    #Label used in place of variable
+                    if(curr_line[2] in list(label_dict.keys())):
+                        error_indices.append([str(i), f"Label {curr_line[2]} misused here", 'f'])
+                    #if the error lies in variable
+                    elif(curr_line[2] not in variable_dict.keys()):
+                        error_indices.append([str(i), curr_line[2], 'b'])
+                        #storing all the variables that are used but not declared
+                        used_variables.append(curr_line[2])
+        
+            #if opcode is of type C
+            elif curr_line[0] == 'mov' and curr_line[2] in register_dict.keys():
+                #If the registers are valid
+                if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
+                    unused_bits = "0" * (5)
+                    machine_code_list.append(op_dict['mov_reg'][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])   
                 else:
                     #creating a string var to store the error message which contains all the registers that are faulty.
                     #This message will be later parsed and printed out as required.
@@ -101,144 +169,74 @@ for i in range(len(input_assembly_codes)):
                         error_message += curr_line[1] + " "
                     if(curr_line[2] not in register_dict.keys()):
                         error_message += curr_line[2] + " "
-                    if(curr_line[3] not in register_dict.keys()):
-                        error_message += curr_line[3] + " "
                     error_indices.append([str(i), error_message, 'a_reg'])
-            else:
-                error_indices.append([str(i),"general syntax error"])
 
-        #If opcode is of type B
-        elif op_dict[curr_line[0]][1] == 'B' and curr_line[2] not in register_dict.keys():
-            imm_flag=True
-            if(eval(curr_line[2][1:])!=int or int(curr_line[2][1:])<0 or int(curr_line[2][1:])>= (2**7)):
-                error_message="illegal immediate value"
-                error_indices.append([str(i),error_message,'e'])
-                imm_flag=False
-            #If the registers are valid
-            if(curr_line[1] in register_dict.keys()):
-                if imm_flag==True:
-                    unused_bits = "0" * (1)
-
-                    
-
-                    immidiate_value = str(bin(int(curr_line[2][1:])))
-                    immidiate_value = "0" * (7-len(immidiate_value[2:])) + immidiate_value[2:]
-                    machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + immidiate_value)
+            elif op_dict[curr_line[0]][1] == 'C' :
+                #If the registers are valid
+                if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
+                    unused_bits = "0" * (5)
+                    machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])
                 else:
-                    pass
-            else:
-                error_indices.append([str(i), curr_line[1], 'a_reg'])
-       
-        #If opcode is of type D
-        elif op_dict[curr_line[0]][1] == 'D':
-            #If the registers are valid and variables are valid
-            if(curr_line[1] in register_dict.keys() and curr_line[2] in variable_dict.keys()):
-                unused_bits = "0" * (1)
-                machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + variable_dict[curr_line[2]])
-            else:
-                #if the error lies in register
-                if(curr_line[1] not in register_dict.keys()):
-                    error_indices.append([str(i), curr_line[1], 'a_reg'])
+                    #creating a string var to store the error message which contains all the registers that are faulty.
+                    #This message will be later parsed and printed out as required.
+                    error_message = ""
+                    if(curr_line[1] not in register_dict.keys()):
+                        error_message += curr_line[1] + " "
+                    if(curr_line[2] not in register_dict.keys()):
+                        error_message += curr_line[2] + " "
+                    error_indices.append([str(i), error_message, 'a_reg'])
             
-                #Label used in place of variable
-                if(curr_line[2] in list(label_dict.keys())):
-                    error_indices.append([str(i), f"Label {curr_line[2]} misused here", 'f'])
-                #if the error lies in variable
-                elif(curr_line[2] not in variable_dict.keys()):
-                    error_indices.append([str(i), curr_line[2], 'b'])
-                    #storing all the variables that are used but not declared
-                    used_variables.append(curr_line[2])
-       
-        #if opcode is of type C
-        elif curr_line[0] == 'mov' and curr_line[2] in register_dict.keys():
-            #If the registers are valid
-            if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
-                unused_bits = "0" * (5)
-                machine_code_list.append(op_dict['mov_reg'][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])   
-            else:
-                #creating a string var to store the error message which contains all the registers that are faulty.
-                #This message will be later parsed and printed out as required.
-                error_message = ""
-                if(curr_line[1] not in register_dict.keys()):
-                    error_message += curr_line[1] + " "
-                if(curr_line[2] not in register_dict.keys()):
-                    error_message += curr_line[2] + " "
-                error_indices.append([str(i), error_message, 'a_reg'])
+            #if op code is of type E
+            elif op_dict[curr_line[0]][1] == 'E':
 
-        elif op_dict[curr_line[0]][1] == 'C' :
-            #If the registers are valid
-            if(curr_line[1] in register_dict.keys() and curr_line[2] in register_dict.keys()):
-                unused_bits = "0" * (5)
-                machine_code_list.append(op_dict[curr_line[0]][0] + unused_bits + register_dict[curr_line[1]] + register_dict[curr_line[2]])
-            else:
-                #creating a string var to store the error message which contains all the registers that are faulty.
-                #This message will be later parsed and printed out as required.
-                error_message = ""
-                if(curr_line[1] not in register_dict.keys()):
-                    error_message += curr_line[1] + " "
-                if(curr_line[2] not in register_dict.keys()):
-                    error_message += curr_line[2] + " "
-                error_indices.append([str(i), error_message, 'a_reg'])
-        
-        #if op code is of type E
-        elif op_dict[curr_line[0]][1] == 'E':
-
-            if(curr_line[1] in label_dict.keys()):
-                unused_bits = "0" * (4)
-                op = op_dict[curr_line[0]][0]
-                mem_ = label_dict[curr_line[1]]           
-                s = op + unused_bits + mem_
-                machine_code_list.append(s)
-            else:
-                if (curr_line[1] in list(variable_dict.keys())):
-                    error_indices.append([str(i), f"Misuse of variable {curr_line[1]} here", 'f'])
+                if(curr_line[1] in label_dict.keys()):
+                    unused_bits = "0" * (4)
+                    op = op_dict[curr_line[0]][0]
+                    mem_ = label_dict[curr_line[1]]           
+                    s = op + unused_bits + mem_
+                    machine_code_list.append(s)
                 else:
-                    error_indices.append([str(i), curr_line[1], 'c'])
-       
-        #if op code is of type F
-        elif op_dict[curr_line[0]][1] == 'F':
-            s = "1001100000000000"
-            machine_code_list.append(s)
-    #Handling case where there are typos in instruction name
-    else:
-        error_indices.append([str(i), "Invalid operation name: " + curr_line[0] , 'a_opcode'])
+                    if (curr_line[1] in list(variable_dict.keys())):
+                        error_indices.append([str(i), f"Misuse of variable {curr_line[1]} here", 'f'])
+                    else:
+                        error_indices.append([str(i), curr_line[1], 'c'])
+        
+            #if op code is of type F
+            elif op_dict[curr_line[0]][1] == 'F':
+                s = "1001100000000000"
+                machine_code_list.append(s)
+        #Handling case where there are typos in instruction name
+        else:
+            error_indices.append([str(i), "Invalid operation name: " + curr_line[0] , 'a_opcode'])
+        
+        
+            if input_assembly_codes[i]=='hlt':
+                hlt_flag=True
+                hlt_counter += 1
+
+    #Handling hlt errors
+    if (hlt_counter <= 1 and input_assembly_codes[-1] != "hlt"):
+        error_indices.append([str(i),"Last instruction not hlt",'i'])
+    elif hlt_counter > 1:
+        error_indices.append([str(i),"Multiple instances of hlt found",'i'])
+
+    #Handling variable declaration error 
+    used_variables = list(set(used_variables))
+    flag_var = True
+    for i in range(len(used_variables)):
+        if(input_assembly_codes[i].split()[0] != 'var'):
+            flag_var = False
+    if not flag_var:
+        error_indices.append(["-","Variables not declared in the beginning","g"])    
     
-    
-        if input_assembly_codes[i]=='hlt':
-            hlt_flag=True
-            hlt_counter += 1
-
-if (hlt_counter <= 1 and input_assembly_codes[-1] != "hlt"):
-    error_indices.append([str(i),"Last instruction not hlt",'i'])
-
-elif hlt_counter > 1:
-    error_indices.append([str(i),"Multiple instances of hlt found",'i'])
-
-
-used_variables = list(set(used_variables))
-flag_var = True
-for i in range(len(used_variables)):
-    if(input_assembly_codes[i].split()[0] != 'var'):
-        flag_var = False
-
-if not flag_var:
-    error_indices.append(["-","Variables not declared in the beginning","g"])    
-    
-
+#printing 
 if(len(error_indices) == 0):
       
           outputfile = open('output.txt','w')
           outputfile.writelines(line + '\n' for line in machine_code_list)
           outputfile.close()
 else:
-        fh2=open('error_bin.txt','w')
-        err_list=[]
-        for i in range(len(error_indices)):
-            output_str=""
-            for j in range(len(error_indices[i])):
-                output_str+=(error_indices[i][j]+" ")
-            err_list.append(output_str)
-        fh2.writelines(line + '\n' for line in err_list)
+    pass
             
   
           
